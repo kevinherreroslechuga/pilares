abd6258e5a0455d59e56ff23c1eb2945878411cc
kevinherreroslechuga <kev.herreros@gmail.com>
2023-08-26 13:39:29 -0600

    pilares

---
+++ actividdad_13.py
@@ -0,0 +1,14 @@
+#Programa para conversion de grados
+
+
+ 
+#Declaracion e inicializacion de variables
+print("Programa para convercion de grados")
+fahrenheit= float(input("Ingrese los grados fahrenheit que desea convertir:"))
+celsius=float
+celsius=(fahrenheit-32)*5/9
+
+
+#Imprime resultado
+print(f"Los grados  fahrenheit: {fahrenheit} convertidos \
+      a Celsius son: {celsius}")
